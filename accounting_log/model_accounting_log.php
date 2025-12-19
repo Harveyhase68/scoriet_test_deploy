7418ccfb675a82cbc1d62d24459e5205e11d7ab8
Alexander Predl <office@predl.cc>
2025-12-20 00:59:49 +0100

    Generated code for my_php_crud_project: predl_template

---
+++ accounting_log/model_accounting_log.php
@@ -0,0 +1,4 @@
+<?php
+echo "Formset.name: laravel_react_primereact";
+echo "Formset.create_edit.button_save.label: Speichern";
+echo "Formset.default_button_color: #2b49de";
---
+++ accounting_log/table_accounting_log.php
@@ -0,0 +1,7 @@
+AAAA = 2
+<?php
+echo "Das ist das Projekt my_php_crud_project";
+BBBB = 3
+echo "Das PHP Script ist hier zu Ende!";
+
+CCCC = 4
