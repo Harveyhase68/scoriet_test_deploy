e3a8c21c9d1c83d9f116d57c5bc1da2cbd89e341
Alexander Predl <office@predl.cc>
2026-01-02 12:18:16 +0100

    Add welcome message and update comments
    Added welcome message at the beginning and updated comments.

--- accounting_log/model_accounting_log.php
+++ accounting_log/model_accounting_log.php
@@ -1,4 +1,11 @@
+<head>
+  <body>
+    Willkommen zu Scoriet! Dies ist am Anfang der Datei!
+  </body>
+</head>
 <?php
 echo "Formset.name: undefined";
+//Hier ist Änderung Mitte
 echo "Formset.create_edit.button_save.label: undefined";
 echo "Formset.default_button_color: undefined";
+//Dies ist eine Änderung zum Schluss!
